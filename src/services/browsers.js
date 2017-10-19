2063c9c69de0413ea18b6a08c2b2a9d8f2907b4f
Ross Basarevych <basarevych@gmail.com>
2017-10-19 13:16:43 +0300

    Browsers service

---
+++ src/services/browsers.js
@@ -0,0 +1,225 @@
+/**
+ * Browsers
+ * @module services/browsers
+ */
+
+/**
+ * Browser and its socket
+ */
+class Browser {
+    /**
+     * Create instance
+     * @param {object} socket                               Web socket
+     */
+    constructor(socket) {
+        this.socket = socket;
+        clear();
+    }
+
+    /**
+     * Socket setter
+     * @param {object} socket
+     * @return {object}
+     */
+    set socket(socket) {
+        this._socket = socket;
+        return this._socket;
+    }
+
+    /**
+     * Socket getter
+     * @type {object}
+     */
+    get socket() {
+        return this._socket;
+    }
+
+    /**
+     * Device setter
+     * @param {number|null} device
+     * @return {number|null}
+     */
+    set device(device) {
+        this._device = device;
+        return this._device;
+    }
+
+    /**
+     * Device getter
+     * @type {number|null}
+     */
+    get device() {
+        return this._device;
+    }
+
+    /**
+     * Card ID setter
+     * @param {string|null} cardId
+     * @return {string|null}
+     */
+    set cardId(cardId) {
+        this._cardId = cardId;
+        return this._cardId;
+    }
+
+    /**
+     * Card ID getter
+     * @return {string|null}
+     */
+    get cardId() {
+        return this._cardId;
+    }
+
+    /**
+     * Card ID timestamp setter
+     * @param {object|null} cardTimestamp
+     * @return {object|null}
+     */
+    set cardTimestamp(cardTimestamp) {
+        this._cardTimestamp = cardTimestamp;
+        return this._cardTimestamp;
+    }
+
+    /**
+     * Card ID timestamp getter
+     * @type {object|null}
+     */
+    get cardTimestamp() {
+        return this._cardTimestamp;
+    }
+
+    /**
+     * User setter
+     * @param {object|null} user
+     * @return {object|null}
+     */
+    set user(user) {
+        this._user = user;
+        return this._user;
+    }
+
+    /**
+     * User getter
+     * @return {object|null}
+     */
+    get user() {
+        return this._user;
+    }
+
+    /**
+     * Target setter
+     * @param {object|null} target
+     * @return {object|null}
+     */
+    set target(target) {
+        this._target = target;
+        return this._target;
+    }
+
+    /**
+     * Target getter
+     * @return {object|null}
+     */
+    get target() {
+        return this._target;
+    }
+
+    /**
+     * Clear all the data
+     */
+    clear() {
+        this.device = null;
+        this.cardId = null;
+        this.cardTimestamp = null;
+        this.user = null;
+        this.target = null;
+    }
+}
+
+/**
+ * Browsers class
+ */
+class Browsers {
+    /**
+     * Create service
+     */
+    constructor() {
+        this.browsers = new Map();
+    }
+
+    /**
+     * Service name is 'browsers'
+     * @type {string}
+     */
+    static get provides() {
+        return 'browsers';
+    }
+
+    /**
+     * Dependencies as constructor arguments
+     * @type {string[]}
+     */
+    static get requires() {
+        return [];
+    }
+
+    /**
+     * This service is a singleton
+     * @type {string}
+     */
+    static get lifecycle() {
+        return 'singleton';
+    }
+
+    /**
+     * Add new browser
+     * @param {string} id
+     * @param {object} socket
+     */
+    add(id, socket) {
+        this.browsers.set(id, new Browser(socket));
+    }
+
+    /**
+     * Tetrieve a browser
+     * @param {string} id
+     * @return {Browser|undefined}
+     */
+    get(id) {
+        return this.browsers.get(id);
+    }
+
+    /**
+     * Remove a browser
+     * @param {string} id
+     */
+    remove(id) {
+        this.browsers.delete(id);
+    }
+
+    /**
+     * Iterator
+     * @return {*}
+     */
+    [Symbol.iterator]() {
+        return this.browsers[Symbol.iterator]();
+    }
+
+    /**
+     * Iterator
+     * @return {*}
+     */
+    keys() {
+        return this.browsers.keys();
+    }
+
+    /**
+     * Iterator
+     * @return {*}
+     */
+    values() {
+        return this.browsers.values();
+    }
+}
+
+module.exports = Browsers;
